8ee9fd6545061efe27430be7c3b0220db0a382ef
Jordan <jordanfurneri57@gmail.com>
2023-05-12 16:47:22 +0200

    add loop + useState for slide

--- src/components/Carrousel.js
+++ src/components/Carrousel.js
@@ -1,21 +1,41 @@
 import React from "react";
 import data from "../data/logements.json";
 import arrow from "../assets/arrow.svg";
+import { useState } from "react";
 
 function Carrousel(props) {
   const { id } = props;
   const apartment = data.find((apartment) => apartment.id === id);
 
-  console.log(apartment.pictures);
+  // console.log(apartment.pictures);
 
-  let actualSlide = 0;
+  const [actualSlide, setCurrentSlide] = useState(0);
+
+  function nextSlide() {
+    if (actualSlide < apartment.pictures.length - 1) {
+      setCurrentSlide(actualSlide + 1);
+    } else {
+      setCurrentSlide(0);
+    }
+  }
+
+  function prevSlide() {
+    if (actualSlide > 0) {
+      setCurrentSlide(actualSlide - 1);
+    } else {
+      setCurrentSlide(apartment.pictures.length - 1);
+    }
+  }
 
   return (
     <div className="carrousel">
       <img src={apartment.pictures[actualSlide]} className="carrousel__slide" />
       <span className="arrow">
-        <img src={arrow} className="arrow__left" />
-        <img src={arrow} className="arrow__right" />
+        <img src={arrow} className="arrow__left" onClick={prevSlide} />
+        <img src={arrow} className="arrow__right" onClick={nextSlide} />
+      </span>
+      <span className="carrousel__counter">
+        {actualSlide + 1}/{apartment.pictures.length}
       </span>
     </div>
   );
