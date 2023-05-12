2b7cb9c895bc78ae4c5e2a4b645c3867fcc15a3f
Jordan <jordanfurneri57@gmail.com>
2023-05-12 09:02:53 +0200

    add App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,18 @@
+import React from "react";
+import Header from "./components/Header";
+import Banner from "./components/Banner.js";
+import Cartcontainer from "./components/Cartcontainer.js";
+import Footer from "./components/Footer.js";
 
+function App() {
+  return (
+    <>
+      <Header />
+      <Banner />
+      <Cartcontainer />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
