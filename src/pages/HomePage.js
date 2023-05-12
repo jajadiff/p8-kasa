8d7150228532f2b223fc761dffe78caf62da5f67
Jordan <jordanfurneri57@gmail.com>
2023-05-12 11:21:02 +0200

    add Card in HomePage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,6 +3,7 @@ import Header from "../components/Header.js";
 import Banner from "../components/Banner.js";
 import Footer from "../components/Footer.js";
 import bgAccueil from "../assets/banner_accueil.png";
+import Card from "../components/Card.js";
 
 function HomePage() {
   return (
@@ -11,6 +12,7 @@ function HomePage() {
       <Banner backgroundImage={bgAccueil}>
         <h2 className="banner__txt">Chez vous, partout et ailleurs</h2>
       </Banner>
+      <Card />
       <Footer />
     </>
   );
