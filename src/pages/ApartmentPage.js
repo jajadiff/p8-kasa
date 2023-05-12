5c1af549c623f51fb3d2c732394170ce6cd7585b
Jordan <jordanfurneri57@gmail.com>
2023-05-12 16:06:51 +0200

    add components Carrousel

--- src/pages/ApartmentPage.js
+++ src/pages/ApartmentPage.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Header from "../components/Header.js";
 import Footer from "../components/Footer.js";
+import { useParams } from "react-router-dom";
+import Carrousel from "../components/Carrousel.js";
 
 function ApartmentPage() {
+  const { id } = useParams();
+
   return (
     <>
       <Header />
+      <Carrousel id={id} />
       <Footer />
     </>
   );
