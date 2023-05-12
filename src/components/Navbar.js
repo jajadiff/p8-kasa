831d0fd12ed9484d83aae4f4764a5faeacf5f0ee
Jordan <jordanfurneri57@gmail.com>
2023-05-12 09:04:04 +0200

    add Navbar components JS

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav>
+      <ul className="header__list">
+        <li>
+          <NavLink to="/">ACCUEIL</NavLink>
+        </li>
+
+        <li>
+          <NavLink to="/about">A PROPOS</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
