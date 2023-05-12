28b9237bbd2e656e09cdd4ab09d23d0c7ecceaf4
Jordan <jordanfurneri57@gmail.com>
2023-05-12 11:03:07 +0200

    add ApartmentPage

---
+++ src/pages/ApartmentPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../components/Header.js";
+import Footer from "../components/Footer.js";
+
+function ApartmentPage() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
+
+export default ApartmentPage;
