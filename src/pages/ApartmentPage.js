997ff26a7a98253b841626ef39ae990508051064
Jordan <jordanfurneri57@gmail.com>
2023-05-31 11:27:55 +0200

    Add error ApartmentPage

--- src/pages/ApartmentPage.js
+++ src/pages/ApartmentPage.js
@@ -4,9 +4,19 @@ import Footer from "../components/Footer.js";
 import { useParams } from "react-router-dom";
 import Carrousel from "../components/Carrousel.js";
 import InformationApartment from "../components/InformationApartment.js";
+import data from "../data/logements.json";
+import ErrorPage from "./ErrorPage.js";
 
 function ApartmentPage() {
   const { id } = useParams();
+
+  const apartmentId = data.find((apartment) => apartment.id === id);
+
+  if (!apartmentId) {
+    console.warn(`No routes matched location "/${id}"`);
+    console.error(`This URL does not exist`);
+    return <ErrorPage />;
+  }
 
   return (
     <>
